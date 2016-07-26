dfe6a18ebce422fa3b3937eefa733ef9cf9c21ac
Caleb Johnson <me@calebj.io>
2016-07-26 01:10:38 -0500

    Mute cog now automatically creates the Mute role

--- mute/mute.py
+++ mute/mute.py
@@ -3,11 +3,14 @@ import asyncio
 import re
 from discord.ext import commands
 from .utils import checks
+import logging
+log = logging.getLogger('red.punish')
 
-TIMESPEC_TRANSLATE = {'s': 1, 'm': 60, 'h': 60*60, 'd': 60*60*24}
+TIMESPEC_TRANSLATE = {'s': 1, 'm': 60, 'h': 60 * 60, 'd': 60 * 60 * 24}
 
 
 class Mute:
+
     def __init__(self, bot):
         self.bot = bot
         self.role_name = "Mute"
@@ -19,23 +22,53 @@ class Mute:
         Time specification is any combination of number with the units s,m,h,d.
         Example: !mute @idiot 1.1h10m Enough bitching already!
         WARNING: if the bot is restarted, the user will have to be unmuted manually."""
+        server = user.server
         role = discord.utils.get(user.server.roles, name=self.role_name)
-        if role:
+        if not role:
+            msg = "The %s role doesn't exist; Creating it now... " % self.role_name
+            msgobj = await self.bot.reply(msg)
+            log.debug('Creating mute role')
             try:
-                await self.bot.add_roles(user, role)
-                time = _parse_time(time)
-                await asyncio.sleep(time)
-                await self.bot.remove_roles(user, role)
-                msg = user.mention + " is no longer muted."
-                if reason:
-                    msg += " Reason for muting was: " + reason
-                else:
-                    msg += " Try to behave from now on."
-                await self.bot.say(msg)
-            except discord.errors.Forbidden:
-                await self.bot.reply("I don't have permissions to modify roles.")
-        else:
-            await self.bot.reply("The {} role doesn't exist; I can't do anything.".format(self.role_name))
+                perms = discord.Permissions.none()
+                # toggle permissions you want, rest are false
+                role = await self.bot.create_role(server, name=self.role_name, permissions=perms)
+                try:
+                    for c in server.channels:
+                        await self.channel_setup(c, role)
+                    await self.bot.edit_message(msgobj, msgobj.content + 'done.')
+                except discord.Forbidden:
+                    await self.bot.say("A error occured while making channel permissions.\nPlease check your channel permissions for the %s role!" % self.role_name)
+            except discord.Forbidden:
+                await self.bot.say("The Manage Roles permission is required to create the %s role." % self.role_name)
+                return
+        try:
+            await self.bot.add_roles(user, role)
+            time = _parse_time(time)
+            await asyncio.sleep(time)
+            await self.bot.remove_roles(user, role)
+            msg = user.mention + " is no longer muted."
+            if reason:
+                msg += " Reason for mute was: " + reason
+            else:
+                msg += " Try to behave from now on."
+            await self.bot.say(msg)
+        except discord.Forbidden:
+            await self.bot.reply("I don't have permission to modify roles.")
+
+    async def new_channel(self, c):
+        r = discord.utils.get(c.server.roles, name=self.role_name)
+        if r:
+            await self.channel_setup(c, r)
+            log.debug('Timeout role created on channel: {}'.format(c.id))
+    
+    async def channel_setup(self, c, role):
+        perms = discord.PermissionOverwrite()
+        if c.type == discord.ChannelType.text:
+            perms.send_messages = False
+            perms.send_tts_messages = False
+        elif c.type == discord.ChannelType.voice:
+            perms.speak = False
+        await self.bot.edit_channel_permissions(c, role, perms)
 
 
 def _parse_time(time):
@@ -56,4 +89,5 @@ def _timespec_sec(t):
 
 def setup(bot):
     n = Mute(bot)
+    bot.add_listener(n.new_channel, 'on_channel_create')
     bot.add_cog(n)
