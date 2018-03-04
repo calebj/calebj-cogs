d697a9998ea51573923728604f89505b30219c8d
Caleb Johnson <me@calebj.io>
2018-03-04 00:59:17 -0600

    [dice] Update with dice v2.2.0 features
    Closes #39

--- dice/dice.py
+++ dice/dice.py
@@ -1,7 +1,19 @@
 from discord.ext import commands
-from .utils import checks
-from .utils.chat_formatting import box, warning
-import dice
+from .utils.chat_formatting import box, warning, pagify
+from pyparsing import ParseBaseException
+
+try:
+    import dice
+except (ImportError, AssertionError):
+    raise ImportError('Please install the dice package from pypi.') from None
+
+DICE_200 = dice.__version__ >= '2.0.0'
+DICE_210 = dice.__version__ >= '2.1.0'
+DICE_220 = dice.__version__ >= '2.2.0'
+
+if DICE_220:
+    from dice import DiceBaseException
+
 
 # Analytics core
 import zlib, base64
@@ -34,7 +46,13 @@ B%B=cR(L=XSj{1GuV5P~u@7tO>@BT#C?1b@;SP9HK!hc$dukO_F$`!QrKmCN_cK}HVkVxm6$S=WAH_Sd
 A37?8a7{(X9Fysa1F~?^KK87nzrr!Qtmh}*HpGHV0FCf}sS%el`_%RB4&POgMZU+J&eb|A1Lc)xrRZdC_Ku=n1WB&^?8dL=""".replace("\n", ""))))
 # End analytics core
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
+
+UPDATE_MSG = ("The version of the dice library installed on the bot (%s) is "
+              "too old for the requested command. Please ask the bot owner "
+              "to update it.\n\nOwners: you can install/update with:\n```\n"
+              "[p]debug bot.pip_install('dice')\n```") % dice.__version__
+
 
 class Dice:
     """A cog which uses the python-dice library to provide powerful dice
@@ -43,41 +61,119 @@ class Dice:
         self.bot = bot
         self.analytics = CogAnalytics(self)
 
-    @commands.command(pass_context=True)
-    async def rd(self, ctx, *, expr: str = 'd20'):
+    @commands.group(pass_context=True, name='dice', invoke_without_command=True)
+    async def _dice(self, ctx, *, expr: str = 'd20'):
         """Evaluates a dice expression. Defaults to roll a d20.
 
         Valid operations include the 'mdn' dice operator, which rolls m dice
         with n sides. If m is omitted, it is assumed to be 1.
         Modifiers include basic algebra, 't' to total a result,
         's' to sort multiple rolls, '^n' to only use the n highest rolls, and
-        'vn' to drop the lowest n rolls. This cog uses the dice library.
+        'vn' to use the lowest n rolls. This cog uses the dice library.
 
-        Examples: 4d20, d100, 6d6v2, 8d4t, 4d4 + 4, 6d8^2"""
+        The full list of operators can be found at:
+        https://github.com/borntyping/python-dice#notation
 
-        try:
-            roll = dice.roll(expr)
-        except dice.ParseException:
-            await self.bot.say(warning('Invalid syntax.'))
+        Examples: 4d20, d100, 6d6v2, 8d4t, 4d4 + 4, 6d8^2
+
+        Note that some commands may be disabled, depending on the version of
+        the dice library that is installed."""
+
+        if ctx.invoked_subcommand is None:
+            await self.roll_common(ctx, expr)
+
+    @_dice.command(pass_context=True, name='min')
+    async def dice_min(self, ctx, *, expr: str = 'd20'):
+        "Evaluates the minimum of an expression."
+
+        if not DICE_200:
+            await self.bot.say(warning(UPDATE_MSG))
             return
-        if type(roll) in [dice.elements.Integer, int]:
-            res = roll
-        elif len(roll) > 0:
-            total = sum(roll)
-            res = ', '.join(map(str, roll))
+
+        await self.roll_common(ctx, expr, dice.roll_min)
+
+    @_dice.command(pass_context=True, name='max')
+    async def dice_max(self, ctx, *, expr: str = 'd20'):
+        "Evaluates the maximum of an expression."
+
+        if not DICE_200:
+            await self.bot.say(warning(UPDATE_MSG))
+            return
+
+        await self.roll_common(ctx, expr, dice.roll_max)
+
+    @_dice.command(pass_context=True, name='verbose')
+    async def dice_verbose(self, ctx, *, expr: str = 'd20'):
+        "Shows the complete breakdown of an expression."
+
+        if not DICE_200:
+            await self.bot.say(warning(UPDATE_MSG))
+            return
+
+        await self.roll_common(ctx, expr, dice.roll, verbose=True)
+
+    async def roll_common(self, ctx, expr, func=dice.roll, verbose=False):
+        try:
+            if DICE_210:
+                roll, kwargs = func(expr, raw=True, return_kwargs=True)
+                result = roll.evaluate_cached(**kwargs)
+            elif DICE_200:
+                roll = func(expr, raw=True)
+                result = roll.evaluate_cached()
+            else:
+                result = func(expr)
+
+        except ParseBaseException as e:
+            msg = warning('An error occured while parsing your expression:\n')
+
+            if DICE_220 and isinstance(e, DiceBaseException):
+                msg += box(e.pretty_print())
+            else:
+                msg += str(e)
+                msg += ('\n\nFor a more detailed explanation, ask the bot '
+                        'owner to update to Dice v2.2.0 or greater.')
+
+            await self.bot.say(msg)
+            return
+
+        if DICE_200 and verbose:
+            if DICE_210:
+                breakdown = dice.utilities.verbose_print(roll, **kwargs)
+            else:
+                breakdown = dice.utilities.verbose_print(roll)
+
+            pages = list(map(box, pagify(breakdown)))
+
+            for page in pages[:-1]:
+                await self.bot.say(page)
+
+        if isinstance(result, int):
+            res = str(result)
+        elif len(result) > 0:
+            total = sum(result)
+            res = ', '.join(map(str, result))
+
             if len(res) > 1970:
                 res = '[result set too long to display]'
-            if len(roll) > 1:
+            if len(result) > 1:
                 res += ' (total: %s)' % total
         else:
-            await self.bot.say('Empty result!')
-            return
-        await self.bot.say(':game_die: %s' % res)
+            res = 'Empty result!'
 
+        res = ':game_die: %s' % res
+
+        if DICE_200 and verbose:
+            if len(res) + len(pages[-1]) >= (2000 - 1):
+                await self.bot.say(pages[-1])
+            else:
+                res = pages[-1] + '\n' + res
+
+        await self.bot.say(res)
 
     async def on_command(self, command, ctx):
         if ctx.cog is self:
             self.analytics.command(ctx)
+
 
 def setup(bot):
     bot.add_cog(Dice(bot))
